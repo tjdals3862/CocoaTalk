7696bb5c925a54918245ae70674d2e343f857811
dui7272 <dui7272@gamil.com>
2023-01-11 12:31:19 +0900

    kojonghoon

--- app/src/main/java/cocoatalk/chat/ChatClient.java
+++ app/src/main/java/cocoatalk/chat/ChatClient.java
@@ -4,6 +4,12 @@ import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Image;
+import java.awt.Graphics2D;
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.GridLayout;
+
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -11,16 +17,22 @@ import java.net.Socket;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JViewport;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.StyleContext;
+import javax.swing.text.StyledDocument;
 
 import java.awt.Font;
 import java.awt.Dimension;
 
-public class ChatClient extends JFrame implements ActionListener, Runnable {
+public class ChatClient extends JFrame implements ActionListener {
   //////////////// 통신과 관련한 전역변수 추가 시작//////////////
   Socket socket = null;
   ObjectOutputStream oos = null;// 말 하고 싶을 때
@@ -32,73 +44,73 @@ public class ChatClient extends JFrame implements ActionListener, Runnable {
 
   Room r = new Room();
 
-  // 이미지 경로 선언
-  String imgPath = "D:\\TEMP\\";
-  ImageIcon imgIcon = new ImageIcon(imgPath + "join.jpg");
+  // 선언
+  String imgPath = "D:\\TEMP\\"; // 배경
+  ImageIcon imgbgIcon = new ImageIcon(imgPath + "kazha.jpg"); //
+  JLabel jlb_bgLabel = new JLabel(imgbgIcon); //
+  JPanel jp = new JPanel(); // 텍스트 메인 페널
+  JPanel jp_send = new JPanel(); // 텍스트 전송 페널
+  JButton jbtn_send = new JButton(new ImageIcon(imgPath + "sendbtn.png")); // 텍스트 전송 버튼
+  JTextField jtf_message = new JTextField(); // 텍스트 입력
+  JTextArea jta_display = new JTextArea();
 
-  JTable jtb = new JTable();
-  JScrollPane jsp = new JScrollPane(jtb);
-  JPanel jp = new JPanel();
-  JPanel jp_behind = new JPanel();
-  JButton jbtn_send = new JButton("보내기");
-  JTextField jtf_message = new JTextField();
-  JTextArea jta_display = new JTextArea()
-
-  {
+  //
+  StyledDocument sd_display = new DefaultStyledDocument(new StyleContext());
+  JTextPane jtp_chatDisplay = new JTextPane(sd_display);
+  JViewport viewport = new JViewport() {
     public void paintComponent(Graphics g) {
-      g.drawImage(imgIcon.getImage(), 0, 0, null);
+      Image img = imgbgIcon.getImage(); //
       setOpaque(false);
+      Graphics2D gd = (Graphics2D) g;
+      gd.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f));
+      g.drawImage(img, 0, 0, this);
       super.paintComponent(g);
     }
   };
-
-  JScrollPane jsp_display = new JScrollPane(jta_display, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-      JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-  Font f = new Font("Paryrus", Font.BOLD, 40);
-  // Font f = new Font("Arial", Font.BOLD, 40);
-  // Font f = new Font("휴먼매직체", Font.BOLD, 40);
-  // Font f = new Font("굴림체", Font.BOLD, 40);
+  //
+  Font font = new Font("Paryrus", Font.BOLD, 40);
+  JScrollPane jsp = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+      JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); //
 
   public void ChatClient() {
 
   }
 
   public void initDisplay() {
-    // 폰트 설정
-    jtf_message.setFont(f);// 입력창 폰트
-    jta_display.setFont(f);// 대화창 폰트
-
-    // 이벤트 처리
-    jbtn_send.addActionListener(this);
-    jtf_message.addActionListener(this);
-
-    // 종료 버튼 누리면 꺼짐
-    this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-    // 창 사이즈 변경 불가
-    this.setResizable(false);
-
-    // 레이아웃 설정
-    this.setLayout(new BorderLayout());
+    jsp.setOpaque(true);
+    jsp.setViewport(viewport);
+    jtp_chatDisplay.setOpaque(false);
+    jsp.setViewportView(jtp_chatDisplay);
+    // 메인 페널
     jp.setLayout(new BorderLayout());
-
-    // 버튼과 텍스트 필드 추가
-    jp.add(jtf_message, BorderLayout.CENTER);
-    jp.add(jbtn_send, BorderLayout.EAST);
-
-    // 텍스트 영역의 테투리
-    jta_display.setLineWrap(true);
-
-    // 스크롤 추가
-    this.add(jsp_display, BorderLayout.CENTER);
-    this.add(jp, BorderLayout.SOUTH);
-
-    // 프레임 설정
-    this.setTitle("COCOA TALK");
-    this.setSize(410, 650);
-    this.setVisible(true);
-
+    jp.add("Center", jsp);
+    jp.add("South", jp_send);
+    // 전송 페널
+    jp_send.setLayout(new BorderLayout());
+    jp_send.add("Center", jtf_message);
+    jp_send.add("East", jbtn_send);
+    // 버튼 크기
+    jbtn_send.setPreferredSize(new Dimension(65, 40));
+    // 전송 화면
+    // jta_display 폰트색상
+    jtp_chatDisplay.setForeground(Color.ORANGE);
+    jtp_chatDisplay.setEditable(false); // 텍스트 필드 입력 불가
+    // 폰트
+    jtp_chatDisplay.setFont(font);
+    jtf_message.setFont(font);
+    // 액션
+    jtf_message.addActionListener(this);
+    jbtn_send.addActionListener(this);
+    // 보내기 버튼 크기 조절
     jbtn_send.setPreferredSize(new Dimension(100, 80));
+    // 채팅장 사이즈 고정
+    this.setResizable(false);
+    this.setLayout(new GridLayout(1, 2));
+    this.add(jp);
+    this.setTitle("Cacao TALK");
+    this.setVisible(true);
+    this.setSize(400, 600);
+    this.setDefaultCloseOperation(DISPOSE_ON_CLOSE); // X 클릭 자동 종료
   }
 
   @Override
@@ -111,6 +123,9 @@ public class ChatClient extends JFrame implements ActionListener, Runnable {
       try {
         oos.writeObject(message);
         jtf_message.setText("");
+        //
+        sd_display.insertString(sd_display.getLength(), message + "\n", null);
+        //
       } catch (Exception e2) {
 
       }
@@ -122,29 +137,16 @@ public class ChatClient extends JFrame implements ActionListener, Runnable {
       try {
         oos.writeObject(message);
         jtf_message.setText("");
+        sd_display.insertString(sd_display.getLength(), message + "\n", null);
       } catch (Exception e2) {
 
       }
+
     }
   }
 
   // 소켓 관련 초기화
   public void init() {
-    Thread th = new Thread(this);
-    th.start();
-  }
-
-  public void chatOpen(String myID, String frID) {
-    id = myID;
-    r.roomSearch(myID, frID);
-    initDisplay();
-    init();
-    jtf_message.setText("나:" + myID + ", 너:" + frID);
-  }
-
-  @Override
-  public void run() {
-    System.out.println("run start");
     try {
       // 서버측의 ip주소 작성하기
       socket = new Socket("192.168.10.74", 3000);
@@ -164,4 +166,22 @@ public class ChatClient extends JFrame implements ActionListener, Runnable {
       System.out.println(e.toString());
     }
   }
+
+  public void chatOpen(String myID, String frID) {
+    id = myID;
+    r.roomSearch(myID, frID);
+    initDisplay();
+    init();
+    jtf_message.setText("나:" + myID + ", 너:" + frID);
+  }
+
+  public void loadChat() {
+
+  }
+  // public static void main(String[] args) {
+  // ChatClient cc = new ChatClient();
+  // cc.initDisplay();
+  // cc.init();
+
+  // }
 }
