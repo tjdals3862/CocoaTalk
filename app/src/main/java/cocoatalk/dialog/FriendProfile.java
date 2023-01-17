25239be7dd63039cf82f43a397bf8820d79e078f
WungsikNam <thanksn20021@gmail.com>
2023-01-17 20:06:57 +0900

    wung0117

--- app/src/main/java/cocoatalk/dialog/FriendProfile.java
+++ app/src/main/java/cocoatalk/dialog/FriendProfile.java
@@ -19,6 +19,7 @@ public class FriendProfile extends JDialog implements ActionListener {
   FriendList fl = null;
   ChatClient cc = null;
   String imgPath = "app\\src\\main\\java\\cocoatalk\\images\\";
+  ImageIcon imageIcon = new ImageIcon(imgPath + "profile.png");
   Toolkit toolkit = Toolkit.getDefaultToolkit();// 로고삽입
   Image img = toolkit.getImage(imgPath + "logo.png");// 로고삽입
   // ImageIcon imageIcon = new ImageIcon(imgPath + "join.jpg");
@@ -45,13 +46,13 @@ public class FriendProfile extends JDialog implements ActionListener {
     this.add(jbtn_del);
     this.add(jp);// 패널추가
     jp.setBackground(new Color(55, 38, 30));// 패널 배경
-    jbtn_chat.setBounds(160, 440, 80, 40);// 1:1채팅버튼
-    jbtn_del.setBounds(70, 440, 80, 40);// 삭제버튼
+    jbtn_chat.setBounds(180, 250, 80, 40);// 1:1채팅버튼
+    jbtn_del.setBounds(65, 250, 80, 40);// 삭제버튼
     jbtn_chat.addActionListener(this); // 1:1 버튼 실행시
     jbtn_del.addActionListener(this); // 삭제 버튼 실행시
 
     setResizable(false); // 창크기 수정불가
-    setSize(330, 550); // 화면사이즈
+    setSize(330, 340); // 화면사이즈
     setVisible(isOpen);
     setTitle(who);// 채팅창 이름(사용자 별로 다름)
     jbtn_del.setBorderPainted(false);// 버튼 테두리 변경
